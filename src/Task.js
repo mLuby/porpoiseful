ebacefe9585899c8e74cb22eff652bef4c314a3d
Mike Luby <mLuby@users.noreply.github.com>
2017-08-16 03:08:08 -0700

    can hold to snooze

--- src/Task.js
+++ src/Task.js
@@ -2,43 +2,81 @@ import React from 'react'
 import moment from "moment"
 import {db} from "./firebase.js"
 import {compose, withState, withHandlers} from "recompose"
+import {Holdable} from "react-touch"
 
 function Task (props) {
-  const {task, editing, toggleEditing, onTitleEdit, onTitleEditSubmit, editedTitle} = props
-
-  return (
-    <li className={`Task${task.completedAt?" completed":""}`} title={task.id}>
-      {editing ? (
-        <form className="editing" onSubmit={onTitleEditSubmit} action="#">
-          <label><span className="icon" role="img" aria-label="editing" onClick={toggleEditing}>📝</span>
-            <input type="text" value={editedTitle} onChange={onTitleEdit} autoFocus onBlur={toggleEditing}/>
+  if (props.editing) {
+    return (
+      <li className={`Task${props.task.completedAt?" completed":""}`} title={props.task.id}>
+        <form className="editing" onSubmit={props.onTitleEditSubmit} action="#">
+          <label><span className="icon" role="img" aria-label="editing" onClick={props.toggleEditing}>📝</span>
+            <input type="text" value={props.editedTitle} onChange={props.onTitleEdit} autoFocus onBlur={props.toggleEditing}/>
           </label>
         </form>
-      ) : (
+      </li>
+    )
+  } else if (props.snoozing) {
+    return (
+      <li className="Task" title={props.task.id}>
+        <span className="icon" role="img" aria-label="close" onClick={props.toggleSnoozing}>✖️</span>
+        <select autoFocus onBlur={props.toggleSnoozing} onChange={props.snooze}>
+          <option value={"N/A"}>How long to snooze?</option>
+          <option value={"15s"}>15s</option>
+          <option value={"later"}>Later (+3h)</option>
+          <option value={"this afternoon"}>This afternoon (1pm)</option>
+          <option value={"this evening"}>This evening (7pm)</option>
+          <option value={"tomorrow morning"}>Tomorrow morning (9am)</option>
+          <option value={"tomorrow afternoon"}>Tomorrow afternoon (1pm)</option>
+          <option value={"tomorrow evening"}>Tomorrow evening (7pm)</option>
+          <option value={"weekend"}>Weekend (Fri 7pm)</option>
+          <option value={"next week"}>next Monday (9am)</option>
+          <option value={"first of the month"}>first of the month (9am)</option>
+          <option value={"clear"}>clear snooze</option>
+        </select>
+      </li>
+    )
+  } else {
+    const classes = [...new Set([
+      "Task",
+      props.task.completedAt && "completed",
+      props.task.snoozeTill && "snoozed",
+    ])].filter(Boolean).join(" ")
+    return (
+      <Holdable onHoldComplete={props.toggleSnoozing}>
+      <li className={classes} title={props.task.id}>
         <div>
-          <input type="checkbox" checked={task.completedAt} onClick={toggleComplete(task)} />
-          <span className="details" onClick={toggleEditing}>
-            {task.title && <span className="title">{task.title}</span>}
-            {task.completedAt && <span className="completedAt">{moment(task.completedAt).fromNow()}</span>}
-            {task.createdAt && <span className="createdAt">{moment(task.createdAt).fromNow()}</span>}
+          <input type="checkbox" checked={props.task.completedAt} onClick={props.toggleComplete} />
+          <span className="details" onClick={props.toggleEditing}>
+            {props.task.title && <span className="title">{props.task.title}</span>}
+            {props.task.completedAt && <span className="completedAt">{moment(props.task.completedAt).fromNow()}</span>}
+            {props.task.createdAt && <span className="createdAt">{moment(props.task.createdAt).fromNow()}</span>}
           </span>
         </div>
-      )}
-    </li>
-  )
+      </li>
+      </Holdable>
+    )
+  }
 }
 
-function toggleComplete (task) {
-  return () => {
-    task.completedAt = task.completedAt ? null : (new Date()).toISOString()
-    console.log(`${task.completedAt ? "" : "un"}completing '${task.title}'`)
-    db.ref('/tasks/'+task.id).set(task)
-  }
+const snoozeOptionToDate = {
+  "N/A": () => { /* noop */},
+  "15s": () => (new Date(Date.now() + 15*1000)).toISOString(),
+  "later": () => (new Date(Date.now() + 3*60*1000)).toISOString(),
+  // "this afternoon": () => (new Date(Date.now() + durationMs)).toISOString(),
+  // "this evening": () => (new Date(Date.now() + durationMs)).toISOString(),
+  // "tomorrow morning": () => (new Date(Date.now() + durationMs)).toISOString(),
+  // "tomorrow afternoon": () => (new Date(Date.now() + durationMs)).toISOString(),
+  // "tomorrow evening": () => (new Date(Date.now() + durationMs)).toISOString(),
+  // "weekend": () => (new Date(Date.now() + durationMs)).toISOString(),
+  // "next week": () => (new Date(Date.now() + durationMs)).toISOString(),
+  // "first of the month": () => (new Date(Date.now() + durationMs)).toISOString(),
+  "clear": () => (new Date()).toISOString(),
 }
 
 export default compose(
   withState('editedTitle', 'setEditTitle', props => props.task.title),
   withState('editing', 'setEditing', false),
+  withState('snoozing', 'setSnoozing', false),
   withHandlers({
     onTitleEdit: props => event => props.setEditTitle(event.target.value),
     onTitleEditSubmit: props => event => {
@@ -54,6 +92,22 @@ export default compose(
     toggleEditing: props => () => {
       props.setEditing(!props.editing)
       props.setEditTitle(props.task.title)
+    },
+    toggleSnoozing: props => () => {
+      props.setSnoozing(!props.snoozing)
+    },
+    snooze: props => event => {
+      const option = event.target.value
+      props.setSnoozing(false)
+      const snoozeTill = snoozeOptionToDate[option]()
+      if (!snoozeTill) return
+      db.ref(`tasks/${props.task.id}/snoozeTill`).set(snoozeTill).then(() => console.log(`snoozed till ${option} ${props.task.id}:${props.task.title}`)).catch(error => console.error(`failed to snooze ${props.task.id}:${props.task.title}`, error))
+    },
+    toggleComplete: props => () => {
+      props.task.completedAt = props.task.completedAt ? null : (new Date()).toISOString()
+      props.task.snoozeTill = null
+      console.log(`${props.task.completedAt ? "" : "un"}completing '${props.task.title}'`)
+      db.ref('/tasks/'+props.task.id).set(props.task)
     },
   })
 )(Task)
--- src/Tasks.js
+++ src/Tasks.js
@@ -2,18 +2,29 @@ import React, {Component} from 'react';
 import {db} from "./firebase.js"
 import Task from "./Task.js"
 
+const CHECK_SNOOZE_MS = 1000
+
 class Tasks extends Component {
   constructor (props) {
     super(props)
     this.state = {tasks: [], filter: t=>t, sort: () => -1}
 
     this.setTasks = this.setTasks.bind(this)
+    this.filterSnoozeTasks = this.filterSnoozeTasks.bind(this)
     this.filterCompletedTasks = this.filterCompletedTasks.bind(this)
-    this.filterUncompletedTasks = this.filterUncompletedTasks.bind(this)
+    this.filterActiveTasks = this.filterActiveTasks.bind(this)
   }
 
   setTasks (tasks) {
     this.setState({tasks})
+  }
+
+  filterSnoozeTasks () {
+    console.log("filter snoozed tasks…")
+    this.setState({
+      filter: t => t.snoozeTill,
+      sort: (t1, t2) => t1.snoozeTill > t2.snoozeTill ? -1 : 1,
+    })
   }
 
   filterCompletedTasks () {
@@ -24,30 +35,42 @@ class Tasks extends Component {
     })
   }
 
-  filterUncompletedTasks () {
-    console.log("filter uncompleted tasks…")
+  filterActiveTasks () {
+    console.log("filter active tasks…")
     this.setState({
-      filter: t => !t.completedAt,
+      filter: t => !t.completedAt && !t.snoozeTill,
       sort: (t1, t2) => t1.createdAt > t2.createdAt ? -1 : 1,
     })
   }
 
   componentWillMount() {
     console.log("loading tasks…")
-    this.filterUncompletedTasks()
+    this.filterActiveTasks()
     db.ref('/tasks').on('value', snapshot => {
       const tasksObj = snapshot.val() || {} // when no tasks it's null
       const tasks = Object.keys(tasksObj).map(k => Object.assign(tasksObj[k], {id: k}))
       console.log("loaded tasks.")
       return this.setTasks(tasks)
     })
+
+    setInterval(checkSnooze.bind(this), CHECK_SNOOZE_MS)
+    function checkSnooze () {
+      const now = (new Date()).toISOString()
+      this.state.tasks.forEach(task => {
+        if (task.snoozeTill < now) {
+          db.ref(`tasks/${task.id}/snoozeTill`).remove().then(() => console.log(`unsnoozed ${task.id}:${task.title}`)).catch(error => console.error(`failed to unsnooze ${task.id}:${task.title}`, error))
+        }
+      })
+    }
   }
 
-  render () { return (
+  render () {
+    return (
     <section className="TaskList">
       <div className="controls">
+        <button onClick={this.filterSnoozeTasks}>Snoozed</button>
+        <button onClick={this.filterActiveTasks}>Active</button>
         <button onClick={this.filterCompletedTasks}>Completed</button>
-        <button onClick={this.filterUncompletedTasks}>Uncompleted</button>
       </div>
       {this.state.tasks.filter(this.state.filter).sort(this.state.sort).map(task => (
         <Task task={task} key={task.id}/>
